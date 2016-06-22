3f3c326f9cf8607a3b13ba0e8edd503dc417ccbf
Tasharn Vidal Brown <tasharn.b@gmail.com>
2016-06-22 15:36:58 +0000

    Add passing email test

--- person.rb
+++ person.rb
@@ -1,13 +1,23 @@
 class Person
-  attr_reader :dob, :first_name, :surname
+  attr_reader :dob, :first_name, :surname, :emails, :phone_numbers
 
   def initialize(fname, sname, dob = nil)
     @first_name = fname.capitalize
     @surname = sname.capitalize
     @dob = Date.parse(dob) if dob
+    @emails = []
+    @phone_numbers = []
   end
 
   def fullname
     "#{@first_name} #{@surname}"
+  end
+
+  def add_email(address)
+    @emails << address
+  end
+
+  def add_phone(number)
+    @phone_numbers << number
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,4 +14,35 @@ describe "Person class" do
     person = Person.new("joe", "bloggs")
     expect(person.dob).to be_nil
   end
+
+  it "should store and return emails in an array" do
+    person = Person.new("joe", "bloggs", "1 Jan 1990")
+
+    expect(person.emails).to eq []
+    person.add_email "joe@foo.com"
+    person.add_email "joe.bloggs@work.com"
+
+    expect(person.emails).to eq ["joe@foo.com", "joe.bloggs@work.com"]
+  end
+
+  it "should store and return phone numbers in an array" do
+    person = Person.new("joe", "bloggs", "1 Jan 1990")
+
+    expect(person.phone_numbers).to eq []
+    person.add_phone "07712345678"
+    person.add_phone "02012345678"
+
+    expect(person.phone_numbers).to eq ["07712345678", "02012345678"]
+  end
+
+  it "should remove the email" do
+    person = Person.new("joe", "bloggs", "1 Jan 1990")
+
+    person.add_email "joe@foo.com"
+    person.add_email "joe.bloggs@work.com"
+
+    person.remove_email(0)
+
+    expect(person.emails).to eq ["joe.bloggs@work.com"]
+  end
 end
